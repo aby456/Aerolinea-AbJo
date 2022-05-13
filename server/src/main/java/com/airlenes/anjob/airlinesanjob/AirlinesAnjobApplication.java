08761f0c94fa7c3e638117675ef915fb233b98bb
jpnavarrete4 <jpnavarrete4@espe.edu.ec>
2022-05-12 22:06:10 -0500

    version-3

--- server/src/main/java/com/airlenes/anjob/airlinesanjob/AirlinesAnjobApplication.java
+++ server/src/main/java/com/airlenes/anjob/airlinesanjob/AirlinesAnjobApplication.java
@@ -5,16 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class AirlinesAnjobApplication {
-
-	private int doraemon;
-
-	print("Hello World");
+	
 	public static void main(String[] args) {
 		SpringApplication.run(AirlinesAnjobApplication.class, args);
 	}
-
-	
-
-
-
 }
