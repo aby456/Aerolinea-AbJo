0548550077075833098e925f78d9f953133faea9
jpnavarrete4 <jpnavarrete4@espe.edu.ec>
2022-05-12 21:59:20 -0500

    pero

---
+++ server/src/main/java/com/airlenes/anjob/airlinesanjob/AirlinesAnjobApplication.java
@@ -0,0 +1,20 @@
+package com.airlenes.anjob.airlinesanjob;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AirlinesAnjobApplication {
+
+	private int doraemon;
+
+	print("Hello World");
+	public static void main(String[] args) {
+		SpringApplication.run(AirlinesAnjobApplication.class, args);
+	}
+
+	
+
+
+
+}
---
+++ server/src/test/java/com/airlenes/anjob/airlinesanjob/AirlinesAnjobApplicationTests.java
@@ -0,0 +1,13 @@
+package com.airlenes.anjob.airlinesanjob;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AirlinesAnjobApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
