a7ab53972be36eadeb1d3829067740143667a202
jpnavarrete4 <jpnavarrete4@espe.edu.ec>
2022-05-18 19:20:20 -0500

    inicio

---
+++ airsalon/src/test/java/com/hairsalon/airsalon/AirsalonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hairsalon.airsalon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AirsalonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
