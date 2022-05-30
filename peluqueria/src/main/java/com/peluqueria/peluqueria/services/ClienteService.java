f5e8610d78fb374c9f5b198f564c4946a71c110d
jpnavarrete4 <jpnavarrete4@espe.edu.ec>
2022-05-30 10:11:14 -0500

    Creacion ClienteController

--- peluqueria/src/main/java/com/peluqueria/peluqueria/services/ClienteService.java
+++ peluqueria/src/main/java/com/peluqueria/peluqueria/services/ClienteService.java
@@ -8,7 +8,7 @@ import com.peluqueria.peluqueria.dto.NewClienteDTO;
 public interface ClienteService {
     public ClienteDTO create(NewClienteDTO clienteDTO);
     public ClienteDTO retrieve(Long id) throws Exception;
-    public ClienteDTO update(ClienteDTO clienteDTO) throws Exception;
+    public ClienteDTO update(ClienteDTO clienteDTO ,Long id) throws Exception;
     public void delete(Long id) throws Exception;
     public List<ClienteDTO> list();
 }
