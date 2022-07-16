218f2a7df234e92fe6954887f7b96dc67fca4956
aby456 <adquillupangui@espe.edu.ec>
2022-07-16 00:11:48 -0500

    exepcion  modificado

---
+++ peluqueria/src/main/java/com/peluqueria/peluqueria/exception/NoContentException.java
@@ -0,0 +1,8 @@
+package com.peluqueria.peluqueria.exception;
+
+public class NoContentException extends RuntimeException {
+
+    public NoContentException(final String message) {
+        super(message);
+    }
+}
