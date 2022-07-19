a33bbd4c9c473e98ce2d016eb41672708f090ee7
PJonathan2001 <jpnavarrete4@espe.edu.ec>
2022-07-18 23:07:39 -0500

    creacion model cliente

---
+++ front-end-peluqueria/src/models/Cliente.ts
@@ -0,0 +1,8 @@
+export default interface IClienteModel {
+    id?: number | null,
+    nombre? : string | null,
+    telefono? : string | null,
+    direccion? : string | null,
+    email : string | null,   
+    password : string | null 
+}
