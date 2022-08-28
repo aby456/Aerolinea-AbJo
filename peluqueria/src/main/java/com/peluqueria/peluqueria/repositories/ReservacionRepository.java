2a90b2dde896d3e689991b8fd56fad9f7f04ef15
aby456 <adquillupangui@espe.edu.ec>
2022-08-28 15:44:42 -0500

    actualizacion backend2.2

--- peluqueria/src/main/java/com/peluqueria/peluqueria/repositories/ReservacionRepository.java
+++ peluqueria/src/main/java/com/peluqueria/peluqueria/repositories/ReservacionRepository.java
@@ -9,5 +9,5 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ReservacionRepository extends JpaRepository<Reservacion,Long> {
-    public List<Reservacion> findByHora(String criteria);
+    public List<Reservacion> findByHorario(String dia);
 }
